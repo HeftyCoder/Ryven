3ac4c7353987593c5a7f5e55793c64afbe038bd9
Georgios Papadoulis <gg.papadoulis@gmail.com>
2024-04-23 14:44:26 +0300

    Update gui_env.py

--- ryven-editor/ryven/gui_env.py
+++ ryven-editor/ryven/gui_env.py
@@ -1,3 +1,3 @@
 # re-exporting the gui_env module from ryven.main.packages.gui_env
 from .main.packages.gui_env import *
-from .main.packages.cognix_library.guis.config.env import *
+from .main.packages.cognix_library.gui.config.env import *
