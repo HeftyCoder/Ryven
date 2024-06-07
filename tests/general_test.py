884bfa057337c99af903c32f1d42d559ffb20144
Georgios Papadoulis <gg.papadoulis@gmail.com>
2024-06-07 19:34:01 +0300

    Update general_test.py

--- tests/general_test.py
+++ tests/general_test.py
@@ -3,9 +3,44 @@ if __name__ != '__main__':
     exit()
 
 import numpy as np
+import pandas as pd
 import time
+import cognixlib.api.data as cl
+t0 = time.perf_counter()
+data = np.array([[1, 2, 3],
+                 [4, 5, 6],
+                 [7, 8, 9],
+                 [10, 11, 12]])
+columns = ['A', 'B', 'C']
 
-a = np.random.rand(10, 10)
-a = np.delete(a, [0, 3, 5], axis=0)
-print(a.shape)
-print(a)
+t1 = time.perf_counter()
+# Create a DataFrame with the NumPy array and additional metadata
+df = pd.DataFrame(data, columns=columns)
+df['Metadata'] = ['meta1', 'meta2', 'meta2', 'meta3'] # this breaks contiguous stuff
+t2 = time.perf_counter()
+
+c_dict = {
+    'meta1': (0, 2),
+    'meta2': (2, 3),
+    'meta3': (3, 4)
+}
+f = cl.FeatureSignal(
+    columns,
+    c_dict,
+    data,
+    None,
+    classes_in_succesion=True
+)
+
+t3 = time.perf_counter()
+
+print(f'Data Creation: {t1-t0}')
+print(f'Pandas Creation: {t2-t1}')
+print(f'Feature Creation: {t3-t2}')
+
+check = np.array_equal(
+    df['A'],
+    f.ldm['A'].data
+)
+print(check)
+print(df.values.flags['C_CONTIGUOUS'])
